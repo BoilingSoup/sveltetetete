ca045f3959d300b001563d8b18bcca60df27de6a
BoilingSoup <boilingsoup@protonmail.com>
2023-08-21 23:41:32 -0700

    fin

---
+++ src/lib/Counter.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+  import { counter } from "./stores";
+</script>
+
+<div style="font-size: {$counter}px;">
+  count is {Math.floor($counter)}
+</div>
+
+<button on:click={counter.increment}>inc</button>
+<button on:click={counter.decrement}>dec</button>
+<button on:click={counter.reset}>reset</button>
+
+<style>
+  div {
+    margin: 1rem;
+    color: gold;
+  }
+</style>
